d18f0393c2b4732dd96ee02e7548d81ff7ab6185
Asiimwe Paul <kpasiimwe@gmail.com>
2023-03-09 16:55:19 +0300

    invulnerabilty_2

--- Scripts/simplePlayer.gd
+++ Scripts/simplePlayer.gd
@@ -7,12 +7,14 @@ const RUN_SPEED = 300
 
 var velocity = Vector2.ZERO
 var state = "running"
+var set_invul = false
 
 onready var animation_player:AnimationPlayer = get_node("AnimationPlayer")
-
+onready var player_animation: AnimatedSprite = get_node("AnimatedSprite2")
+onready var invul_timer = get_node("invulTimer")
 
 func _ready():
-	animation_player
+	_invincibility()
 
 func _physics_process(delta):
 	
@@ -45,5 +47,16 @@ func update_position(delta):
 	velocity.y = move_and_slide(velocity, Vector2.UP).y
 
 func _invincibility():
-	
+	invul_timer.start()
+	animation_player.play("invincibility")
 	pass
+
+
+
+
+func _on_invulTimer_timeout():
+	set_invul = true
+	if set_invul == true:
+		animation_player.stop()
+		player_animation.visible = true
+	pass # Replace with function body.
